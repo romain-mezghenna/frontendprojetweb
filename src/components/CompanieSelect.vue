c3bd27977a7ff902cb778904ce6e7363bf984e5d
romain-mezghenna <romain.mezghenna@etu.umontpellier.fr>
2022-03-30 22:03:19 +0200

    done

---
+++ src/components/CompanieSelect.vue
@@ -0,0 +1,31 @@
+<template>
+    <select class="form-select" v-model="this.internalValue">
+        <option disabled value="">Veuillez choisir</option>
+        <option v-for="option in options" :key="option.idCompanie" v-bind:value="option.idCompanie">{{option.nomCompanie}}</option>
+    </select>
+</template>
+
+<script>
+const axios = require('axios')
+const API_URL = 'https://astroworld-api.herokuapp.com/';
+export default {
+    name:'CompanieSelect',
+    props : {},
+    data(){
+        return {
+            options : [],
+            internalValue : '',
+        }
+    },
+    mounted(){
+        axios.get(API_URL + 'companies').then((response) => {
+            this.options = response.data
+        });
+    },
+    
+}
+</script>
+
+<style>
+
+</style>
