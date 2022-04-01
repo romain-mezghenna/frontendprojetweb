d2d9ab0d2f05409edba152a79c3f5a8e03649404
romain-mezghenna <romain.mezghenna@etu.umontpellier.fr>
2022-04-01 08:27:10 +0200

    redirect if not autorized

--- src/router/index.js
+++ src/router/index.js
@@ -12,14 +12,6 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
-  },
-  {
     path:'/login',
     name:'Login',
     component: Login
@@ -82,5 +74,18 @@ const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  const publicPages = ['/', '/login', '/register', '/astres', '/ships', '/companies'];
+  const authRequired = !publicPages.includes(to.path);
+  const loggedIn = localStorage.getItem('user');
+  // trying to access a restricted page + not logged in
+  // redirect to login page
+  if (authRequired && !loggedIn) {
+    next('/login');
+  } else {
+    next();
+  }
+});
 
 export default router
